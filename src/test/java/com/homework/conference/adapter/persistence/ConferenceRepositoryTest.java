602211681be9a55e681618d431d45f6754033d07
vladyslavpalamarchuk <v.palamarchuk9@gmail.com>
2022-06-01 09:49:09 +0300

    add simple persistence test example

---
+++ src/test/java/com/homework/conference/adapter/persistence/ConferenceRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.homework.conference.adapter.persistence;
+
+import com.github.database.rider.spring.api.DBRider;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@DBRider
+@ActiveProfiles("test")
+class ConferenceRepositoryTest {
+
+    @Autowired
+    private ConferenceRepository repository;
+
+    @Nested
+    class ExistByTests {
+        @Test
+        void whenNoConferences_thenExistingByNameFalse() {
+            assertThat(repository.existsByName("unknown")).isFalse();
+        }
+    }
+
+}
