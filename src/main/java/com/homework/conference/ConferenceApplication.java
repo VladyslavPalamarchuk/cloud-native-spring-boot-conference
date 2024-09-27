c62ec3d9b2f1186f79f854219219ca82143911f7
VladyslavPalamarchuk <65964034+VladyslavPalamarchuk@users.noreply.github.com>
2024-09-27 20:25:39 +0300

    remove usless comment

--- src/main/java/com/homework/conference/ConferenceApplication.java
+++ src/main/java/com/homework/conference/ConferenceApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConferenceApplication {
-    // gateway чи це норм варіант для мепінгу в адаптор персістенса. Або як правильно робити. І чи норм що гейтвей буде класом, чи варто робити aka GatewayImpl
-    //
 
     public static void main(String[] args) {
         SpringApplication.run(ConferenceApplication.class, args);
